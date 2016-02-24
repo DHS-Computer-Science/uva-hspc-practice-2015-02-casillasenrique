e546acfb86c78926f5bd3db8d7263ed2df667351
casillasenrique <casillasenrique@live.com>
2016-02-24 08:28:25 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,41 @@
 
 public class  {
   public static void main(String[] args) {
-
+//Scanner
+      Scanner in = new Scanner(System.in);
+      //number of entries
+      int entries = in.nextInt();
+      in.nextLine();
+      String word;
+      //all the rounded letters and nums
+      String rounded = "BCDGJOPQRSU2356890";
+      
+      //for loop for entries
+      for(int i = 0; i<entries; i++)
+      {
+         word = in.nextLine();
+         int wordLen = word.length();
+         //used to check if all the letters are rounded
+         int round = 0;
+         for(int j = 0; j<wordLen;j++)
+         {
+            //checks if the letter is a rounded letter or not
+            if(rounded.indexOf(word.charAt(j)) == -1)
+            {
+               System.out.println("NOT ROUNDED");
+               j = wordLen-1;
+            }
+            else
+            {
+               round++;
+            }
+            if(round == wordLen)
+            {
+               System.out.println("ROUNDED");
+            }
+         
+         }
+      }      
     System.exit(0);
   }
 }
